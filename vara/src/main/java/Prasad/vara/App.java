3573c84081a91b0036920d3a6c5fc8d2e831e55e
prasad <vprasad.java@gmail.com>
2017-12-31 13:06:52 -0500

    testing vara

---
+++ vara/src/main/java/Prasad/vara/App.java
@@ -0,0 +1,13 @@
+package Prasad.vara;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
